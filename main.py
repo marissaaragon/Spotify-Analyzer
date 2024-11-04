c1cf3912b944cb90ca64e0642c871da14d86aa86
Marissa Aragon <marag27@wgu.edu>
2024-11-04 15:16:30 -0700

    Updated menu aesthetics

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ else:
             st.write("You are already authorized. Here are your top tracks.")
 
     if sp:
-        menu = st.sidebar.radio("Go to", ["Top Tracks", "Top Artists"])
+        menu = st.selectbox("Select a section", ["Top Tracks", "Top Artists"])
 
         if menu == "Top Tracks":
             # Get user top tracks and popularity
