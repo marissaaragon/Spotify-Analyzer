5ffadec1516ea232e960850a02315807d2fcff38
Marissa Aragon <marag27@wgu.edu>
2024-11-04 15:22:51 -0700

    Updated authorization methods

--- main.py
+++ main.py
@@ -13,14 +13,12 @@ load_dotenv()
 # Get variables from env
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = "https://spotify-analyzer-777.streamlit.app"  # Redirect URI should point to the main app
+REDIRECT_URI = "https://spotify-analyzer-777.streamlit.app"
 
 # Check if CLIENT_ID and CLIENT_SECRET are loaded correctly
 if CLIENT_ID is None or CLIENT_SECRET is None:
     st.error("Missing CLIENT_ID or CLIENT_SECRET. Please check your environment variables.")
 else:
-    st.write("Starting Spotify authentication process...")
-
     # Spotify API authentication
     sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
                             client_secret=CLIENT_SECRET,
@@ -35,7 +33,7 @@ else:
         try:
             token_info = sp_oauth.get_access_token(auth_code[0], as_dict=False)
             sp = spotipy.Spotify(auth=token_info)
-            st.write("Authorization successful! You can now see your top tracks.")
+            st.success("Authorization successful! You can now see your data.")
         except Exception as e:
             st.error(f"Error: {e}")
         sp = None
@@ -47,21 +45,19 @@ else:
             sp = None
         else:
             sp = spotipy.Spotify(auth=token_info['access_token'])
-            st.write("You are already authorized. Here are your top tracks.")
 
     if sp:
+        # Create a sidebar dropdown navigation menu
         menu = st.sidebar.selectbox("Select a section", ["Top Tracks", "Top Artists"])
 
         if menu == "Top Tracks":
+            st.header("Your Top Tracks")
             # Get user top tracks and popularity
             top_tracks = sp.current_user_top_tracks(limit=20)
             # Create DataFrame
             tracks_data = [{'name': track['name'], 'popularity': track['popularity']} for track in top_tracks['items']]
             df_tracks = pd.DataFrame(tracks_data)
-
             # Display top tracks in Streamlit
-            st.title("Spotify Analyzer")
-            st.header("Your Top Tracks")
             st.dataframe(df_tracks)
 
             # Plot popularity of top tracks
@@ -70,11 +66,10 @@ else:
             st.pyplot(fig)
 
         elif menu == "Top Artists":
+            st.header("Your Top Artists")
             # Get top artists and images
             top_artists = sp.current_user_top_artists(limit=10)
             artist_data = [{'name': artist['name'], 'image_url': artist['images'][0]['url']} for artist in top_artists['items']]
-
             # Display top artists in Streamlit
-            st.header("Your Top Artists")
             for artist in artist_data:
                 st.image(artist['image_url'], caption=artist['name'], width=150)
